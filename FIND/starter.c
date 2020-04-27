fa9160606e1ba87e6819fc8a1222c8bf2d04a139
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2020-04-27 04:03:06 -0500

    added memory trimmer

--- FIND/starter.c
+++ FIND/starter.c
@@ -2,7 +2,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
 
 
 
@@ -10,9 +11,20 @@
 
 
 
+char * trimmer(char *array){
+    char * temp; int i = 0; temp = array;
+    for(i = 0; array[i] != NULL; i++);
+    free(array);
+    array = (char*)malloc((i+1)*sizeof(char));
+    for(int j = 0; j < i;j++){
+        array[j] = temp[j];
+    }
+}
+
+
 int find(const char *WTL){
-    DIR* dot; struct dirent* nextdir;
-    DIR* dot1;
+    DIR* dot; struct dirent* nextdir; char lastSlash[1] = "/";
+    DIR* dot1; char filenamer[100] = "/"; char* dirName; char *nexter;
     if(WTL == NULL){ //if no directory specified, check current dir
         dot = opendir("."); //open buffer at current directory  
     }
@@ -23,23 +35,27 @@ int find(const char *WTL){
         printf("Cannot open"); //indicated directory could not be opened
     }
     //nextdir = readdir(dot + 1);
-    // nextdir = readdir(dot);
-    // nextdir = readdir(dot);
+    nextdir = readdir(dot);
+    nextdir = readdir(dot); 
     while ((nextdir = readdir(dot)) != NULL){ //check next address
         if(nextdir->d_type == ISDIR){ //if it is a directory check it
-            printf("hello");
-            dot1 = opendir(nextdir->d_name); //open buffer at directory
-            //find(nextdir->d_name);//call find at subdirectory
+            nexter = (char*)malloc((strlen(nextdir->d_name))*sizeof(char));
+            dirName = nextdir->d_name;
+            nexter = strcat(filenamer, dirName); 
+            nexter = strcat(nexter, lastSlash);
+            printf("\n %s", nexter);
+            //find(nexter);
+            //find(strcat(nextdir->d_name, temp));//call find at subdirectory
         }
-        printf("%s ", nextdir->d_name); //print entry name
-        printf("%u \n", nextdir->d_type); //directory is 4
+        printf("\n %s ", nextdir->d_name); //print entry name
     }
     return 0;
 }
 
 int main(){
-    char str1[20];
-
+    char str1[20] = "hello";
+    trimmer(str1);
+    printf("%s", str1);
     find(NULL);
     scanf("%s", str1);
     return 0;
